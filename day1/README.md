e5c1e18eb0993150b13c879ecc416bc02924d813
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2021-08-15 12:21:34 +1200

    Add readme and try using an iterator for dealing with the move issue (instead of reference, althoguh ultimately is a reference anyway).
    Ran clippy on code. Still having trouble getting nvim to show clippy

---
+++ day1/README.md
@@ -0,0 +1,21 @@
+## Day 1
+
+We have a list of numbers, and need to find some of them who sum to 2020
+
+### Part A
+
+Find 2 numbers who sum to 2020, then return their product
+
+### Part B
+
+Find 3 numbers who sum to 2020, then return their product
+
+### Tests
+
+None for this one currently.
+
+### Additional thoughts?
+
+I was passing the data into the function twice, and only cared about reading it, so needed to use a reference.
+
+Could I have converted to an iter before I sent it to the function? Then no move would have happened? Worth a try
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,13 +6,13 @@ fn main() -> Result<()> {
     let s = include_str!("../input_data.txt");
     let lines = s.split('\n');
     let numbers: Vec<i32> = lines.map(str::parse::<i32>).map(Result::unwrap).collect();
-    let (a, b) = get_pairs_from_numbers(&numbers).unwrap();
+    let (a, b) = get_pairs_from_numbers(numbers.iter()).unwrap();
     println!(
-        r#"part a: Our numbers are {:?} which multiply to {}"#,
+        "part a: Our numbers are {:?} which multiply to {}",
         (a, b),
         a * b
     );
-    let (a, b, c) = get_triples_from_numbers(&numbers).unwrap();
+    let (a, b, c) = get_triples_from_numbers(numbers.iter()).unwrap();
     println!(
         "part b: Our numbers are {:?} which multiply to {}",
         (a, b, c),
@@ -21,16 +21,12 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn get_pairs_from_numbers(numbers: &Vec<i32>) -> Option<(&i32, &i32)> {
-    numbers
-        .into_iter()
-        .tuple_combinations()
-        .find(|(a, b)| *a + *b == TOTAL)
+fn get_pairs_from_numbers(numbers: std::slice::Iter<i32>) -> Option<(&i32, &i32)> {
+    numbers.tuple_combinations().find(|(a, b)| *a + *b == TOTAL)
 }
 
-fn get_triples_from_numbers(numbers: &Vec<i32>) -> Option<(&i32, &i32, &i32)> {
+fn get_triples_from_numbers(numbers: std::slice::Iter<i32>) -> Option<(&i32, &i32, &i32)> {
     numbers
-        .into_iter()
         .tuple_combinations()
         .find(|(a, b, c)| *a + *b + *c == TOTAL)
 }
