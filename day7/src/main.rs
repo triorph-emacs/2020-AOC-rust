434774fda327837bc0bd78361c925786e22dc0f0
Michael Walsh <Michael.Walsh@canterbury.ac.nz>
2021-11-07 16:50:19 +1300

    Get day a working for day7

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -9,6 +9,34 @@ pub struct Bag {
     colour: String,
     contains: Vec<BagQuantity>,
 }
+fn find_parents_for_colour<'a>(bags: &'a [Bag], bag_to_calculate: &str) -> Vec<&'a str> {
+    let mut ret: Vec<&'a str> = vec![];
+    for bag in bags.iter() {
+        for child_bag in bag.contains.iter() {
+            if child_bag.colour == bag_to_calculate {
+                ret.push(&bag.colour[..]);
+            }
+        }
+    }
+    ret
+}
+fn calculate_day_a(bags: &[Bag], bag_to_calculate: &str) -> usize {
+    let mut count = 0;
+    let mut bags_found: Vec<&str> = vec![bag_to_calculate];
+
+    while count < bags_found.len() {
+        let bag = bags_found[count];
+        let new_bags = find_parents_for_colour(bags, bag);
+        for new_bag in new_bags.iter() {
+            if !bags_found.contains(new_bag) {
+                bags_found.push(new_bag);
+            }
+        }
+        count += 1;
+    }
+
+    count - 1 // don't include the starting bag
+}
 
 // Parse the each input line into a Bag object.
 // Bags have a colour, and can contain other bags of given colours.
@@ -45,17 +73,23 @@ peg::parser! { grammar bag_parser() for str {
         = colour:colour() (" bags contain ") contains:(no_other_bags() / inner_bags()) {
             Bag{ colour, contains }
         }
-
 }}
 
 fn main() {
-    println!("Hello, world!");
-    println!("test");
+    let lines: &str = include_str!("../input_data.txt");
+    let bags: Vec<Bag> = lines
+        .lines()
+        .map(bag_parser::parse)
+        .map(Result::unwrap)
+        .collect();
+    println!("Day a result: {}", calculate_day_a(&bags, "shiny gold"));
 }
 
 #[cfg(test)]
 mod test {
     use crate::bag_parser;
+    use crate::calculate_day_a;
+    use crate::Bag;
     #[test]
     fn test_parse_bag() {
         let bag = bag_parser::parse("green bags contain 2 red bags, 4 blue bags.");
@@ -67,6 +101,7 @@ mod test {
             assert_eq!(bag.contains[1].colour, "blue");
             assert_eq!(bag.contains[1].quantity, 4);
         } else {
+            // consider testing with `cargo test --features peg/trace`
             panic!("bag had an error, {:?}", bag);
         }
     }
@@ -95,5 +130,16 @@ mod test {
         } else {
             panic!("bag had an error, {:?}", bag);
         }
+    }
+
+    #[test]
+    fn test_day_a_problem() {
+        let lines: &str = include_str!("../test_data_a.txt");
+        let bags: Vec<Bag> = lines
+            .lines()
+            .map(bag_parser::parse)
+            .map(Result::unwrap)
+            .collect();
+        assert_eq!(calculate_day_a(&bags, "shiny gold"), 4);
     }
 }
